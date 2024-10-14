936d010d34f5b3e26ba95ca9f2f4d5ab9aa94d1d
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2024-10-14 21:41:25 +0600

    spread operator

--- es6.js
+++ es6.js
@@ -7,3 +7,23 @@ const student = {
 
 const about = `My name is ${student.name}. I am ${student.age} years old and attended ${student.experience[1]}`
 console.log(about)
+
+//arrow function
+const getFiftyFive = ()=>55;
+const addSixtyFive = num => num+65;
+const isEven = x => x%2==0;
+console.log(isEven(23))
+const addThree = (x,y,z)=> x+y+z;
+
+const doMath = (num1,num2) =>{
+  const sum = num1+num2
+  return sum 
+}
+result = doMath(5,23)
+console.log(result)
+
+//spread operator
+const newNumbers = [...numbers]
+newNumbers.push(100)
+console.log(numbers)
+console.log(newNumbers)
